556fad8cf89890f6251579520a48cb2fa5748dd3
MohammedAlaeSerr <serroukhealae@gmail.com>
2024-01-12 15:27:25 -0800

    Without libraries what have we? We have no past and no future

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isupper - Check if a character in an uppercase letter
+ * @c: The character to be checked
+ *
+ * Return: Return 1 if a c is an uppercase letter, 0 otherwise.
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
