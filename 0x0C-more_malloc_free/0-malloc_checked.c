93d12e4935af1a60412cb5c62153c0ee1413a21e
MohammedAlaeSerr <serroukhealae@gmail.com>
2024-01-18 10:54:03 -0800

    MORE MALLOC, FREE

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - cause normal process termination with a status value of 98
+ * @b: allocated memory
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *array;
+
+	array = malloc(b);
+	if (array == NULL)
+		exit(98);
+	return (array);
+
+}
