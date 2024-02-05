82d152c59966d536bebadd26d15901c69aaaf0f1
Mohamed Alae <serroukhealae@gmail.com>
2024-02-05 10:05:38 -0800

    Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_list - the number of elements in a linked list.
+ * @h: head pointer
+ *
+ * Return: the number of elms of the linked list.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t couter = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		counter++;
+	}
+
+	return (cou
